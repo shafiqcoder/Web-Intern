92235abceaeea4f808b736cc8268bce9f218c87b
shafiqcoder <shafiq4w8@gmail.com>
2021-11-09 00:09:02 +0500

    beginner-javascript notes

--- JS Function.js
+++ JS Function.js
@@ -16,10 +16,22 @@ function inchToCm(inches){
     const cm=inches*2.54
     return cm
 };
+function makeAbaby(first,last){
+    const baby= {
+        name: `${first}${last}`,
+        age:0,
+    }
+    return baby
+}
+
 //Arrow functions
 const inchToCm=inches=>inches*2.54;
+const add=(a,b=3)=>a+b;                                                                         
+const makeABaby=(first,last)=>({name:`${first}${last}`,age:0}) //round brackets are used to return object implicitly, it wll not think its block scope.
+//IIFE
+//Immediately invoked function expression.
 
+(function(age){
+    return `I am cool and my age is ${age}`
+})(27); // auto runs
 
-
-
-const add=(a,b=3)=>a+b;
